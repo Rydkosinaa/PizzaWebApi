398fa82dae5ecb1e0006fada449130ee04ecac7f
MariiaGumenyk <marusik.gum@gmail.com>
2023-01-18 18:46:52 +0200

    134

--- PizzaApi/Controllers/PizzaController.cs
+++ PizzaApi/Controllers/PizzaController.cs
@@ -1,46 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PizzaApi.Models;
 using System.Xml.Linq;
-using PizzaApi.Models;
 using System.Threading.Tasks;
 namespace PizzaApi.Controllers
 {
     [Route("api/[controller]")]
     public class PizzaController : Controller
     {
-       
-        public IActionResult Index()
-        {
-            return View();
-        }
 
-
-        //private static List<Pizza> pizzas = new()
+        //public IActionResult Index()
         //{
-        //    new Pizza(){ PizzaPrice =18, rating=0, Name="Margarita",PizzaIngredients={Ingredient.Cheese, Ingredient.Tomato, Ingredient.Dough }},
-        //    new Pizza(){ PizzaPrice =26, rating=0, Name="Peperoni",PizzaIngredients={ Ingredient.Onion,Ingredient.Cheese, Ingredient.Tomato, Ingredient.Peperoni, Ingredient.Dough }},
-        //    new Pizza(){ PizzaPrice =31, rating=0, Name="Mushroom",PizzaIngredients={ Ingredient.Mushrooms, Ingredient.Onion,Ingredient.Cheese, Ingredient.Tomato, Ingredient.Peperoni, Ingredient.Dough }},
-        //    new Pizza(){ PizzaPrice =39, rating=0, Name="Mozarella",PizzaIngredients={ Ingredient.Mozarella, Ingredient.Onion,Ingredient.Cheese, Ingredient.Tomato, Ingredient.Peperoni, Ingredient.Dough }},
-        //    new Pizza(){ PizzaPrice =34, rating=0, Name="Vegitariana",PizzaIngredients={ Ingredient.Mushrooms, Ingredient.Onion, Ingredient.Tomato,  Ingredient.Dough }},
+        //    return View();
+        //}
 
-        //};
-        private static List<Ingredient> ingredients = new()
+
+        private static List<Pizza> pizzas = new()
         {
-        new Ingredient() {Name="Dough",Price= 10},
-         new Ingredient() {Name="Peperoni", Price=1},
-        new Ingredient() {Name="Onion", Price=2},
-         new Ingredient() {Name="Cheese",Price= 3 },
-        new Ingredient() {Name="Tomato", Price=5},
-        new Ingredient() {Name="Mozarella", Price=15 },
-        new Ingredient() {Name="Peper", Price=12},
-        new Ingredient() {Name="Salad", Price=17 },
-        new Ingredient(){Name="Salmon",Price= 17},
-        new Ingredient(){Name="Olives",Price= 12 }
-    };
+            new Pizza(){ PizzaPrice =18, rating=0, Name="Margarita",PizzaIngredients={Ingredient.Cheese, Ingredient.Tomato}},
+            new Pizza(){ PizzaPrice =26, rating=0, Name="Peperoni",PizzaIngredients={ Ingredient.Onion,Ingredient.Cheese, Ingredient.Tomato, Ingredient.Peperoni}},
+            new Pizza(){ PizzaPrice =31, rating=0, Name="Mushroom",PizzaIngredients={ Ingredient.Mushrooms, Ingredient.Onion,Ingredient.Cheese, Ingredient.Tomato, Ingredient.Peperoni }},
+            new Pizza(){ PizzaPrice =39, rating=0, Name="Mozarella",PizzaIngredients={ Ingredient.Mozarella, Ingredient.Onion,Ingredient.Cheese, Ingredient.Tomato, Ingredient.Peperoni}},
+            new Pizza(){ PizzaPrice =34, rating=0, Name="Vegitariana",PizzaIngredients={ Ingredient.Mushrooms, Ingredient.Onion, Ingredient.Tomato}},
+
+        };
+        //    private static List<Ingredient> ingredients = new()
+        //    {
+        //    new Ingredient() {Name="Dough",Price= 10},
+        //     new Ingredient() {Name="Peperoni", Price=1},
+        //    new Ingredient() {Name="Onion", Price=2},
+        //     new Ingredient() {Name="Cheese",Price= 3 },
+        //    new Ingredient() {Name="Tomato", Price=5},
+        //    new Ingredient() {Name="Mozarella", Price=15 },
+        //    new Ingredient() {Name="Peper", Price=12},
+        //    new Ingredient() {Name="Salad", Price=17 },
+        //    new Ingredient(){Name="Salmon",Price= 17},
+        //    new Ingredient(){Name="Olives",Price= 12 }
+        //};
 
         [HttpGet]
-        public IEnumerable<Ingredient> Get() =>ingredients;
-
+        //public IEnumerable<Ingredient> Get() =>ingredients;
+        public IEnumerable<Pizza> Get() => pizzas;
+        [HttpGet("{name}")]
+        public IActionResult Get(string name)
+        {
+            var pizza = pizzas.SingleOrDefault(p => p.Name == name);
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+            return Ok(pizza);
+        }
 
     }
 }
--- PizzaApi/Models/Pizza.cs
+++ PizzaApi/Models/Pizza.cs
@@ -3,15 +3,15 @@
     public class Pizza
     {
 
-        public double PizzaPrice { get; set; }/* = Ingredient.Dough.Price*/
-        public int rating = 0;
-        public string Name = "Custom pizza";
+        public double PizzaPrice { get; set; } = Ingredient.Dough.Price;
+        public int rating { get; set; } = 0;
+        public string Name { get; set; } = "Custom pizza";
 
 
-       //public List<Ingredient> PizzaIngredients = new List<Ingredient>()
-       //{
-       //      Ingredient.Dough
-       //};
+        public List<Ingredient> PizzaIngredients { get; set; } = new List<Ingredient>()
+        {
+              Ingredient.Dough
+        };
 
         //public Pizza(List<Ingredient> pizzaIngredients, string name)
         //{
@@ -31,21 +31,21 @@
 
         //}
 
-        //public void AddIngredient(Ingredient ingredient)
-        //{
+        public void AddIngredient(Ingredient ingredient)
+        {
 
-        //    this.PizzaIngredients.Add(ingredient);
-        //    this.PizzaPrice += ingredient.Price;
-        //}
+            this.PizzaIngredients.Add(ingredient);
+            this.PizzaPrice += ingredient.Price;
+        }
 
-        //public bool RemoveIngredient(Ingredient ingredient)
-        //{
-        //    this.PizzaPrice -= ingredient.Price;
-        //    return this.PizzaIngredients.Remove(ingredient);
-        //    //this.PizzaIngredients.Remove(ingredient);
-        //    //this.PizzaPrice -= ingredient.Price;
-        //    //return this.PizzaPrice;
-        //}
+        public bool RemoveIngredient(Ingredient ingredient)
+        {
+            this.PizzaPrice -= ingredient.Price;
+            return this.PizzaIngredients.Remove(ingredient);
+            //this.PizzaIngredients.Remove(ingredient);
+            //this.PizzaPrice -= ingredient.Price;
+            //return this.PizzaPrice;
+        }
 
 
     }
